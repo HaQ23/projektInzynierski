05765a0ccf600f132c9da27e0326267c7b12e38c
Krzysztof Dobosz <dobosz.krzysiek7@gmail.com>
2024-04-28 12:49:50 +0200

    created footer component

--- src/webapp/src/app/modules/core/core.module.ts
+++ src/webapp/src/app/modules/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SharedModule } from '../shared/shared.module';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, FooterComponent],
   imports: [CommonModule, SharedModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, FooterComponent],
 })
 export class CoreModule {}
