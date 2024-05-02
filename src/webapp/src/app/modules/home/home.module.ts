bf33821768389cb3a5478d9ade60266306a6aa21
Krzysztof Dobosz <dobosz.krzysiek7@gmail.com>
2024-05-02 12:14:14 +0200

    changes

--- src/webapp/src/app/modules/home/home.module.ts
+++ src/webapp/src/app/modules/home/home.module.ts
@@ -5,10 +5,9 @@ import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeRoutingModule } from './home-routing.modules';
-import { OurServicesComponent } from './our-services/our-services.component';
 
 @NgModule({
-  declarations: [HeaderComponent, HomeComponent, AboutUsComponent, OurServicesComponent],
+  declarations: [HeaderComponent, HomeComponent, AboutUsComponent],
   imports: [CommonModule, SharedModule, HomeRoutingModule],
   exports: [HomeComponent],
 })
