29d8c79c7dfa75b85b14acfdd16392cf3da8a524
Krzysztof Dobosz <dobosz.krzysiek7@gmail.com>
2024-12-31 15:59:29 +0100

    build project

---
+++ src/webapp/dist/webapp/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-CJDMYMUZ.mjs";import"./chunk-ZSWMZNGA.mjs";import"./chunk-7QWHGEEN.mjs";import"./chunk-LXPPITVW.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
